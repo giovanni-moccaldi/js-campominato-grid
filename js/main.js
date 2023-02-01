07d57320889e319e543cf2b6506d3e93888a1a1e
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-01 22:43:22 +0100

    with comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// const bottone = document.getElementById("play")
+// bottone.addEventListener(
+//     "click",
+//     function () {
+//         this.value.toggle();
+//     }
+// )
 const listtowrite = document.getElementById("big-square");
 // assegno numeri in una funzione per 100
 generate(listtowrite);
@@ -7,13 +14,14 @@ function generate() {
     for (let i = 0; i < 100; i++) {
         whitelist.push(i);
         // console.log(whitelist);
-
-
     }
 
     for (let i = 0; i < 100; i++) {
+        // creo un div per mettere i numeri
         const listitem = document.createElement("div");
+        // qui scrivo i numeri nel html
         listitem.innerHTML = whitelist[i];
+        // aggiungo una classe
         listitem.classList.add("smallsquare");
         listtowrite.append(listitem);
         // aggiungo un click
