b0e984e53497043bfcc18a3a9dedf3bab8fcaa59
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-01 21:36:15 +0100

    numbers1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
-const quadro = document.getElementById("big-square");
-for (let i = 0; i < 100; i++) {
-    const element = document.createElement("div");
-    element.classList.add("smallsquare");
-    quadro.append("smallsquare");
-}
+const listtowrite = document.getElementById("big-square");
 // assegno numeri in una funzione per 100
-generate(quadro);
-function generate(quadro) {
+generate(listtowrite);
+function generate() {
     // assegno i valori
     const whitelist = [];
     for (let i = 0; i < 100; i++) {
@@ -16,5 +11,11 @@ function generate(quadro) {
 
     }
 
+    for (let i = 0; i < 100; i++) {
+        const listitem = document.createElement("li");
+        listitem.innerHTML = whitelist[i];
+        listitem.classList.add("smallsquare");
+        listtowrite.append(listitem);
+    }
 
 }
