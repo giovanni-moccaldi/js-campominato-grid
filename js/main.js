7f0d08e04a0e6b306eb9780f308dfdf0e6f1a3fe
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-01 18:16:02 +0100

    ..

--- js/main.js
+++ js/main.js
@@ -4,23 +4,23 @@ generate(quadro);
 function generate(quadro) {
     // assegno i valori
     const whitelist = [];
-    random(0, whitelist.length);
+    random(0, 100);
     for (let i = 0; 1 > 100; i++) {
         whitelist.push(i + 1);
 
 
     }
     console.log(whitelist);
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i > 100; i++) {
         // console.log(quadro)
         // poi genero un div e una classe
         const element = document.createElement("div");
-        element.classList.add("small-square");
+        element.classList.add("smallsquare");
 
     }
-    quadro.append("small-square")
+    quadro.append("smallsquare")
 
-}
-function random() {
-    const numbers = Math.floor(Math.random() * 10) + 1;
+    function random() {
+        const numbers = Math.floor(Math.random() * 100) + 1;
+    }
 }
