0fc898441be78de93117a9f05cedbd1e22165a4f
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-01 15:20:56 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const quadro = document.getElementById("big-square");
 // assegno numeri in una funzione per 100
+
 function generate(quadro) {
     for (let i = 0; i < 100; i++) {
         // poi genero un div e una classe
         const element = document.createElement("div");
         element.classList.add("small-square");
         quadro.append("smallsquare");
+
 
 
     }
