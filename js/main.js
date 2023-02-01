730f7cae3ecd7d64f6471e0aee1c53f470494163
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-01 16:45:20 +0100

    numbers

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 const quadro = document.getElementById("big-square");
 // assegno numeri in una funzione per 100
-
+generate(quadro);
 function generate(quadro) {
     for (let i = 0; i < 100; i++) {
+        // console.log(quadro)
         // poi genero un div e una classe
         const element = document.createElement("div");
-        element.classList.add("small-square");
-        quadro.append("smallsquare");
-
+        element.classList.add("smallsquare");
+        // assegno i valori
+        const numbershtml = random(0, 100);
+        console.log(numbershtml);
 
 
     }
 
+}
+function random(quadro) {
+    const numbers = Math.floor(Math.random() * 10) + 1;
 }
