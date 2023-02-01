2f3180b887146f076207c53e577ad309e09fe87e
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-01 20:16:03 +0100

    cannot

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@ const quadro = document.getElementById("big-square");
 // assegno numeri in una funzione per 100
 generate(quadro);
 function generate(quadro) {
+    random(0, 100);
     // assegno i valori
     const whitelist = [];
-    random(0, 100);
-    for (let i = 0; 1 > 100; i++) {
+    for (let i = 0; i > 100; i++) {
         whitelist.push(i + 1);
 
 
     }
     console.log(whitelist);
     for (let i = 0; i > 100; i++) {
-        // console.log(quadro)
+
         // poi genero un div e una classe
         const element = document.createElement("div");
         element.classList.add("smallsquare");
+        quadro.append("smallsquare");
 
     }
-    quadro.append("smallsquare")
 
     function random() {
         const numbers = Math.floor(Math.random() * 100) + 1;
